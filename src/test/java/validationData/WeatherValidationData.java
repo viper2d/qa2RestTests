49f15398f37dea31340180088e31a5cb30ebb928
viper2d <viper2d@inbox.lv>
2017-11-23 16:26:06 +0200

    up

--- src/test/java/validationData/WeatherValidationData.java
+++ src/test/java/validationData/WeatherValidationData.java
@@ -8,19 +8,23 @@ public class WeatherValidationData {
 
     public WeatherValidation getWeatherValidationData(Integer id) {
 
+        //Expected "weather" data, according to documentation.
+
         HashMap<Integer, WeatherValidation> weatherConditions = new HashMap<Integer, WeatherValidation>();
 
         weatherConditions.put(300, new WeatherValidation(300, "Drizzle", "light intensity drizzle", "09d"));
-        weatherConditions.put(301, new WeatherValidation(301, "Drizzle", "main desctiption", "09d"));
-        weatherConditions.put(302, new WeatherValidation(302, "Drizzle", "main desctiption", "09d"));
-        weatherConditions.put(303, new WeatherValidation(303, "Drizzle", "main desctiption", "09d"));
-        weatherConditions.put(304, new WeatherValidation(304, "Drizzle", "main desctiption", "09d"));
-        weatherConditions.put(305, new WeatherValidation(305, "Drizzle", "main desctiption", "09d"));
+        weatherConditions.put(301, new WeatherValidation(301, "Drizzle", "drizzle", "09d"));
+        weatherConditions.put(302, new WeatherValidation(302, "Drizzle", "heavy intensity drizzle ", "09d"));
+        weatherConditions.put(310, new WeatherValidation(310, "Drizzle", "light intensity drizzle rain ", "09d"));
+        weatherConditions.put(311, new WeatherValidation(311, "Drizzle", "drizzle rain", "09d"));
+        weatherConditions.put(312, new WeatherValidation(312, "Drizzle", "heavy intensity drizzle rain", "09d"));
 
         return weatherConditions.getOrDefault(id, new WeatherValidation());
     }
 
     public HashMap<String, Integer> getPressureRange() {
+
+        //Expanded expected "pressure" range, according to 3 year statistics.
 
         HashMap<String, Integer> pressureRange = new HashMap<>();
 
